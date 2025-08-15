1cfb6190cafd088c2ce4aa130c4d5c235bcab8d5
sancheza <sancheza@gmail.com>
2025-08-15 18:52:26 -0400

    Stop tracking shell scripts

--- run_forward_pe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Full path to Python and script
-PY="/opt/homebrew/bin/python3"
-#SCRIPT="/Users/asanchez/dev/financial-analytics/get_forwardpe.py"
-SCRIPT="$HOME/dev/financial-analytics/get_forwardpe.py"
-
-# Kill all inherited env vars to break out of LibreOffice's Python
-exec env -i "$PY" "$SCRIPT" "$1"
--- run_get_dividend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Full path to Python and script
-PY="/opt/homebrew/bin/python3"
-SCRIPT="$HOME/dev/financial-analytics/get_dividend.py"
-
-# Kill all inherited env vars to break out of LibreOffice's Python
-exec env -i "$PY" "$SCRIPT" "$1"
